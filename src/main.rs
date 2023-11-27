50a00118ee2a55e299c06533fcd7ffabe5b7e3c1
ryo2357 <ryosuke.m.ab@gmail.com>
2023-11-27 14:48:21 +0900

    可視化の完了

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use chrono::{DateTime, Local, Utc};
+use chrono::{DateTime, Local};
 use futures::stream;
 use influxdb2::models::DataPoint;
 use influxdb2::Client;
-use influxdb2_derive::WriteDataPoint;
+use rand::Rng;
 use tokio::sync::mpsc;
 use tokio::time::{Duration, Instant};
 
@@ -25,22 +25,52 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-#[derive(Default, WriteDataPoint)]
-#[measurement = "temp_sensor_data"]
-struct TempSensorData {
-    #[influxdb(tag)]
-    unit: Option<String>,
-    #[influxdb(field)]
-    sensor_1: f64,
-    #[influxdb(field)]
-    sensor_2: f64,
-    #[influxdb(field)]
-    sensor_3: f64,
-    #[influxdb(timestamp)]
+// #[derive(Default, WritePoint)]
+// #[measurement = "machine_1"]
+// struct TempSensorData {
+//     #[influxdb(tag)]
+//     sensor_type: Option<String>,
+//     #[influxdb(field)]
+//     tempureture_1: f64,
+//     #[influxdb(field)]
+//     tempureture_2: f64,
+//     #[influxdb(field)]
+//     tempureture_3: f64,
+//     #[influxdb(timestamp)]
+//     time: i64,
+// }
+fn generate_tempurature_data_point(
+    tempureture_1: f64,
+    tempureture_2: f64,
+    tempureture_3: f64,
     time: i64,
+) -> anyhow::Result<DataPoint> {
+    let point = DataPoint::builder("machine_1")
+        .tag("sensor_type", "tempurature")
+        .field("tempureture_1", tempureture_1)
+        .field("tempureture_2", tempureture_2)
+        .field("tempureture_3", tempureture_3)
+        .timestamp(time)
+        .build()?;
+    Ok(point)
 }
 
-async fn generate_data(tx: mpsc::Sender<Vec<TempSensorData>>) -> anyhow::Result<()> {
+// #[derive(Default, WritePoint)]
+// #[measurement = "machine_1"]
+// struct DistanceSensorData {
+//     #[influxdb(tag)]
+//     sensor_type: Option<String>,
+//     #[influxdb(field)]
+//     distance_1: f64,
+//     #[influxdb(field)]
+//     distance_2: f64,
+//     #[influxdb(field)]
+//     distance_3: f64,
+//     #[influxdb(timestamp)]
+//     time: i64,
+// }
+
+async fn generate_data(tx: mpsc::Sender<Vec<DataPoint>>) -> anyhow::Result<()> {
     // データ生成処理
     // 50msごとにデータを生成してチャンネルに送信
     // 200データ⇒10s毎にtxに送信×60⇒10分分のデータ
@@ -49,32 +79,23 @@ async fn generate_data(tx: mpsc::Sender<Vec<TempSensorData>>) -> anyhow::Result<
     let mut field3 = 50.0;
     let mut next_loop_start_time = Instant::now();
 
-    for _ in 0..60 {
-        let mut points: Vec<TempSensorData> = Vec::<TempSensorData>::new();
+    // for _ in 0..60 {
+    loop {
+        let mut points: Vec<DataPoint> = Vec::<DataPoint>::new();
         for _ in 0..200 {
+            // println!("{},{},{}", field1, field2, field3);
             next_loop_start_time += Duration::from_millis(50);
+            let time = Local::now().timestamp_nanos_opt().unwrap();
 
-            // let dt: DateTime<Local> = Local::now();
-            // let timestamp: i64 = dt.timestamp();
-            // let point = DataPoint::builder("stat")
-            //     .tag("unit", "temperature")
-            //     .field("field1", field1)
-            //     .field("field2", field2)
-            //     .field("field3", field3)
-            //     .timestamp(timestamp)
-            //     .build()?;
-            let point = TempSensorData {
-                unit: Some("machine_1".to_owned()),
-                sensor_1: field1,
-                sensor_2: field2,
-                sensor_3: field3,
-                time: Local::now().timestamp_nanos_opt().unwrap(),
-            };
+            let point = generate_tempurature_data_point(field1, field2, field3, time)?;
             points.push(point);
 
-            field1 += (rand::random::<i32>() % 200 - 100) as f64 / 10.0;
-            field2 += (rand::random::<i32>() % 200 - 100) as f64 / 10.0;
-            field3 += (rand::random::<i32>() % 200 - 100) as f64 / 10.0;
+            {
+                let mut rng = rand::thread_rng();
+                field1 += rng.gen_range(-100..=100) as f64 / 10.0;
+                field2 += rng.gen_range(-100..=100) as f64 / 10.0;
+                field3 += rng.gen_range(-100..=100) as f64 / 10.0;
+            }
 
             let now = Instant::now();
             if next_loop_start_time > now {
@@ -88,7 +109,7 @@ async fn generate_data(tx: mpsc::Sender<Vec<TempSensorData>>) -> anyhow::Result<
     Ok(())
 }
 
-async fn send_data(mut rx: mpsc::Receiver<Vec<TempSensorData>>) -> anyhow::Result<()> {
+async fn send_data(mut rx: mpsc::Receiver<Vec<DataPoint>>) -> anyhow::Result<()> {
     // クライアント設定
     let host = std::env::var("INFLUXDB_HOST").unwrap();
     let org = std::env::var("INFLUXDB_ORG").unwrap();
@@ -116,37 +137,3 @@ async fn send_data(mut rx: mpsc::Receiver<Vec<TempSensorData>>) -> anyhow::Resul
 
     Ok(())
 }
-
-// async fn _bing_fn() {
-//     let host = std::env::var("INFLUXDB_HOST").unwrap();
-//     let org = std::env::var("INFLUXDB_ORG").unwrap();
-//     let token = std::env::var("INFLUXDB_TOKEN").unwrap();
-//     let bucket = "bucket"; // Specify your bucket name
-
-//     let client = Client::new(host, org, token);
-
-//     let mut field1 = 50.0;
-//     let mut field2 = 50.0;
-//     let mut field3 = 50.0;
-
-//     // 100ms毎にデータを生成
-//     // 5s毎に纏めてデータを送信
-//     //
-//     for _ in 0..180 {
-//         let point = DataPoint::builder("stat")
-//             .tag("unit", "temperature")
-//             .field("field1", field1)
-//             .field("field2", field2)
-//             .time(DateTime::from(Utc::now()))
-//             .build()
-//             .unwrap();
-
-//         client.write(bucket, vec![point]).await.unwrap();
-
-//         field1 += (rand::random::<i32>() % 200 - 100) as f64 / 10.0;
-//         field2 += (rand::random::<i32>() % 200 - 100) as f64 / 10.0;
-//         field3 += (rand::random::<i32>() % 200 - 100) as f64 / 10.0;
-
-//         tokio::time::sleep(Duration::from_secs(10)).await;
-//     }
-// }
